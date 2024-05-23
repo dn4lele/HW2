947e02f29cb101dffe58cea9e4d718eec6b93435
dn4lele <danielmaier4312@gmail.com>
2024-05-23 16:06:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw2/UsersActivity.java
@@ -0,0 +1,29 @@
+package com.example.hw2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hw2.databinding.ActivityUsersBinding;
+
+public class UsersActivity extends AppCompatActivity {
+
+    //binding
+    ActivityUsersBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_users);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
